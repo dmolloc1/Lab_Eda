eebbbcc2fd13b9d8f647ffe3c6884fdb28aa97ef
dmolloc1 <dmolloc@unsa.edu.pe>
2024-05-21 15:30:08 -0500

    Primera logica para los numeros de movimientos

--- Lab02/Ejercicio04.java
+++ Lab02/Ejercicio04.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ejercicio04 {
-    
+    static ArrayList<Integer> sol = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(torresDeHanoi(num));
+        //Formula recursiva f(n) =  2F(n - 1) + 1
+
+    }
+
+    public static int torresDeHanoi(int n){
+        if(n == 1){
+            return 1;
+        }
+        return  1 + 2 *torresDeHanoi(n - 1);
+    }
 }
--- Lab02/Ejercicio05.java
+++ Lab02/Ejercicio05.java
@@ -4,22 +4,31 @@
 import java.util.Scanner;
 
 public class Ejercicio05 {
+    static int medio ;
     public static void main(String[] args) {
         
         int[] num = {12,34,63,72,74,530,2123};
-        int resp = busquedaBinaria(num, num.length/ 2,num.length, 0, 34);
-        System.out.println("se encontro el numero " + 3 + "en el indice "+ resp +"y vale "+ num[resp]);
+        int resp = busquedaBinaria(num,num.length, 0, 44);
+        if(resp == -1){
+            System.out.println("No se encontro el numero");
+        }else{
+            System.out.println("se encontro el numero " + 3 + "en el indice "+ resp +"y vale "+ num[resp]);
+        }
     }
 
-    public static int busquedaBinaria(int[] arr, int medio, int max, int min, int buscar){
-        if(arr[medio] == buscar){
-            return medio;
+    public static int busquedaBinaria(int[] arr, int max, int min, int buscar){
+        if(min <= max){
+            medio = (max + min)/ 2;
+            if(arr[medio] == buscar){
+                return medio;
+            }
+            if(arr[medio] < buscar){
+                min = medio + 1;
+            }else{
+                max = medio - 1;
+            }
+            return busquedaBinaria(arr,max,min,buscar);
         }
-        if(arr[medio] < buscar){
-            min = medio;
-        }else{
-            max = medio;
-        }
-        return busquedaBinaria(arr, (min + max)/ 2,max,min,buscar);
+        return -1;
     }
 }
