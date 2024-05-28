6dc24bc7afd55d220fea8557df6de7d5e95cb9c6
dmolloc1 <dmolloc@unsa.edu.pe>
2024-05-27 19:20:40 -0500

    .

--- Lab03/EjerciciosClase/Ejercicio2.java
+++ Lab03/EjerciciosClase/Ejercicio2.java
@@ -1,5 +1,4 @@
 public class Ejercicio2{
-    static int mitada;
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(multiplicarArray(0, arr.length - 1, arr));
