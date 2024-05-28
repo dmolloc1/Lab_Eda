4843a5f2706391865927cf89c7c3c76af5ef5ee3
dmolloc1 <dmolloc@unsa.edu.pe>
2024-05-27 19:02:33 -0500

    Se agregaron los ejercicio 1 y 2 de la tarea

---
+++ Lab03/Propuestos/Ejercicio1.java
@@ -0,0 +1,24 @@
+public class Ejercicio1{
+    static int mitada;
+    public static void main(String[] args) {
+        int[] arr1 = {11, 2, 4, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = {11, 2, 4, 4, 5, 6, 7, 8, 10};
+        System.out.println(compararArrays(arr1, arr2, 0, arr1.length - 1));
+    }
+    public static boolean compararArrays(int[] arr1, int[] arr2, int inicio, int fin){
+        if(arr1.length != arr2.length){
+            System.out.println("Los arrays no son de igual longitud");
+            return false;
+        }
+        if(inicio == fin){
+            return arr1[inicio] == arr2[inicio];
+        }else{
+            int medio = (inicio + fin) / 2;
+            boolean p1 = compararArrays(arr1, arr2, inicio, medio);
+            boolean p2 = compararArrays(arr1, arr2, medio + 1, fin );
+        
+            return p1 && p2;
+       
+        }
+    }
+}
---
+++ Lab03/Propuestos/Ejercicio2.java
@@ -0,0 +1,32 @@
+public class Ejercicio2{
+    static int mitada;
+    public static void main(String[] args) {
+        int[] arr = {11, 22, 4, 74, 5, 16, 700, 8, 10, 1000, 0};
+        System.out.println("El número menor del array es : " + minimo(arr, 0, arr.length - 1));
+        System.out.println("El número mayor del array es : " + maximo(arr, 0, arr.length - 1));
+    }
+    public static int minimo(int[] arr, int inicio, int fin){
+        if(inicio == fin){
+            return arr[inicio];
+        }else{
+            int medio = (inicio + fin) / 2;
+            int p1 = minimo(arr, inicio, medio);
+            int p2 = minimo(arr, medio + 1, fin);
+        
+            return Math.min(p1, p2);
+       
+        }
+    }
+    public static int maximo(int[] arr, int inicio, int fin){
+        if(inicio == fin){
+            return arr[inicio];
+        }else{
+            int medio = (inicio + fin) / 2;
+            int p1 = maximo(arr, inicio, medio);
+            int p2 = maximo(arr, medio + 1, fin);
+        
+            return Math.max(p1, p2);
+       
+        }
+    }
+}
